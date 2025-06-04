03f486bb705b2543cb657f7d1322878bf907e6a0
GrasshopperBears <starday456@naver.com>
2025-06-05 03:05:47 +0900

    footer 추가

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -33,22 +33,28 @@ function BaghChal() {
                 winner={winner}
                 setWinner={setWinner}
             />
-            <div style={{ marginTop: 10 }}>
-                {winner && <h3>{winner === 'G' ? '염소 승리!' : '호랑이 승리!'}</h3>}
-                <b>턴:</b> {turn === 'G' ? '염소' : '호랑이'}
-                <br />
-                <b>놓인 염소:</b> {goatsPlaced} / 20
-                <br />
-                <b>잡힌 염소:</b> {capturedGoats}
-            </div>
-            <div ref={scriptElement} style={{ width: '900px', height: '100px' }}>
-                <ins
-                    className='kakao_ad_area'
-                    style={{ display: 'none' }}
-                    data-ad-unit='DAN-8OF5DE5VnWGYbB0K'
-                    data-ad-width='728'
-                    data-ad-height='90'
-                ></ins>
+            <div className='footer'>
+                <div style={{ marginTop: 10 }} className='footer-info'>
+                    <span>{winner && <h3>{winner === 'G' ? '염소 승리!' : '호랑이 승리!'}</h3>}</span>
+                    <span style={{ fontSize: '30px' }}>
+                        <b>턴:</b> {turn === 'G' ? '염소' : '호랑이'}
+                    </span>
+                    <span>
+                        <b>놓인 염소:</b> {goatsPlaced} / 20
+                    </span>
+                    <span>
+                        <b>잡힌 염소:</b> {capturedGoats}
+                    </span>
+                </div>
+                <div ref={scriptElement}>
+                    <ins
+                        className='kakao_ad_area'
+                        style={{ display: 'none' }}
+                        data-ad-unit='DAN-8OF5DE5VnWGYbB0K'
+                        data-ad-width='728'
+                        data-ad-height='90'
+                    ></ins>
+                </div>
             </div>
         </div>
     );
